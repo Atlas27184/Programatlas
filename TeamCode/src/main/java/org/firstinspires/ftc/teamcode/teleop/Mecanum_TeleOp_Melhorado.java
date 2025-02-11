5fb8bed20d1cc7c04dabbfb185ed58c69ebe7e6d
Atlas-ftc <atlas.ftc@outlook.com>
2025-02-11 10:37:09 -0300

    Versão 2.1

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/Mecanum_TeleOp_Melhorado.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/Mecanum_TeleOp_Melhorado.java
@@ -3,23 +3,22 @@ package org.firstinspires.ftc.teamcode.teleop;
 import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.IMU;
-import com.qualcomm.robotcore.hardware.Servo;
 
 import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 import org.firstinspires.ftc.robotcore.external.navigation.YawPitchRollAngles;
 
 
-@TeleOp(name = "Programação braba")
-public class Mecanum_TeleOp extends LinearOpMode {
+@TeleOp(name = "Programação braba 2")
+public class Mecanum_TeleOp_Melhorado extends LinearOpMode {
 
     DcMotor superior_esquerdo;
     DcMotor superior_direito;
     DcMotor inferior_esquerdo;
     DcMotor inferior_direito;
+    DcMotor gira_slide;
     IMU imu;
 
     private double targetAngle;
@@ -33,6 +32,7 @@ public class Mecanum_TeleOp extends LinearOpMode {
         superior_direito = hardwareMap.get(DcMotor.class, "superior direito");
         inferior_esquerdo = hardwareMap.get(DcMotor.class, "inferior esquerdo");
         inferior_direito = hardwareMap.get(DcMotor.class, "inferior direito");
+        gira_slide = hardwareMap.get(DcMotor.class, "gira_slide");
         imu = hardwareMap.get(IMU.class, "imu");
 
 
@@ -40,7 +40,7 @@ public class Mecanum_TeleOp extends LinearOpMode {
         IMU.Parameters parameters = new IMU.Parameters(
                 new RevHubOrientationOnRobot(
                         RevHubOrientationOnRobot.LogoFacingDirection.UP,
-                        RevHubOrientationOnRobot.UsbFacingDirection.FORWARD
+                        RevHubOrientationOnRobot.UsbFacingDirection.LEFT
                 )
         );
 
@@ -53,6 +53,7 @@ public class Mecanum_TeleOp extends LinearOpMode {
         superior_esquerdo.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         inferior_direito.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         inferior_esquerdo.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        gira_slide.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
 
 
 
@@ -64,6 +65,7 @@ public class Mecanum_TeleOp extends LinearOpMode {
             while (opModeIsActive()) {
                 drive();
                 telemetry.update();
+                gira_slide.setPower(gamepad2.left_stick_y);
             }
         }
     }
@@ -71,10 +73,15 @@ public class Mecanum_TeleOp extends LinearOpMode {
         double y, x, rx;
         double kP = 0.03;
         y = gamepad1.left_stick_y * 0.8;
-        x = gamepad1.right_stick_x * 0.8;
-        rx = (gamepad1.right_trigger - gamepad1.left_trigger) * 0.8;
+        x = gamepad1.left_stick_x * 0.8;
+        rx = gamepad1.right_stick_x * 0.8;
 
         double currentAngle = getHeading();
+
+        double botHeading = imu.getRobotYawPitchRollAngles().getYaw(AngleUnit.RADIANS);
+
+        double rotX = x * Math.cos(botHeading) - y * Math.sin(botHeading);
+        double rotY = x * Math.sin(botHeading) + y * Math.cos(botHeading);
 
         if (rx != 0) {
             targetAngle = currentAngle;
@@ -102,10 +109,12 @@ public class Mecanum_TeleOp extends LinearOpMode {
         double error = normalizeAngle(targetAngle - currentAngle);
         double correction = (rx == 0) ? error * kP : 0;
 
-        superior_esquerdo.setPower(y - x + correction - rx);
-        inferior_esquerdo.setPower(y + x + correction - rx);
-        superior_direito.setPower(y + x - correction + rx);
-        inferior_direito.setPower(y - x - correction + rx);
+        double denominator = Math.max(Math.abs(rotY) + Math.abs(rotX) + Math.abs(rx) + Math.abs(correction), 1);
+
+        superior_esquerdo.setPower((rotY - rotX + correction - rx) / denominator);
+        inferior_esquerdo.setPower((rotY + rotX + correction - rx) / denominator);
+        superior_direito.setPower((rotY + rotX - correction + rx) / denominator);
+        inferior_direito.setPower((rotY - rotX - correction + rx) / denominator);
 
         telemetry.addData("CurrentAngle:", currentAngle);
         telemetry.addData("TargetAngle:", targetAngle);
